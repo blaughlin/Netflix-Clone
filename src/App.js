d682aebf569506d3ee61aec8352a550b7ee18d82
Bernard Laughlin <bernardlaughlin@gmail.com>
2021-06-14 07:10:55 -0800

    added player and search

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import React from 'react'
-import { BrowserRouter as Router, Switch } from 'react-router-dom'
-import  {Home, Browse, Signin, Signup} from './pages'
-import * as ROUTES from './constants/routes'
-import { IsUserRedirect, ProtectedRoute} from './helpers/routes'
-import { useAuthListener } from './hooks'
+import React from 'react';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
+import { Home, Browse, Signin, Signup } from './pages';
+import * as ROUTES from './constants/routes';
+import { IsUserRedirect, ProtectedRoute } from './helpers/routes';
+import { useAuthListener } from './hooks';
+
 export default function App() {
-  const  user  = useAuthListener();
-  console.log(user)
+  const { user } = useAuthListener();
+
   return (
     <Router>
       <Switch>
-        <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE}  path={ROUTES.SIGN_IN}>
+        <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_IN}>
           <Signin />
         </IsUserRedirect>
         <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_UP}>
@@ -26,4 +27,3 @@ export default function App() {
     </Router>
   );
 }
-
--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,8 +1,9 @@
 import React, { useContext, useState, useEffect } from 'react'
+import Fuse from 'fuse.js'
 import SelectProfileContainer from './profiles'
 import { FirebaseContext } from '../context/firebase'
 import { FooterContainer } from './footer'
-import { Card, Header, Loading } from '../components'
+import { Card, Header, Loading, Player } from '../components'
 import * as ROUTES from '../constants/routes'
 import logo from '../logo.svg'
 
@@ -27,6 +28,18 @@ export function BrowseContainer({slides}) {
         setSlideRows(slides[category])
     }, [slides, category])
 
+    useEffect(() => {
+        const fuse = new Fuse(slideRows, {
+            keys: ['data.description', 'data.title', 'data.genre'],
+        })
+        const results = fuse.search(searchTerm).map(({item}) => item)
+        
+        if (slideRows.length > 0 && searchTerm.length > 3 && results.length >0){
+            setSlideRows(results)
+        } else {
+            setSlideRows(slides[category])
+        }
+    }, [searchTerm])
     return profile.displayName ? (
         <>
         {loading ? <Loading src={user.photoURL} /> : <Loading.ReleaseBody />}
@@ -88,7 +101,10 @@ export function BrowseContainer({slides}) {
                         ))}
                     </Card.Entities>
                     <Card.Feature category={category}>
-                        <p>Hello</p>
+                        <Player>
+                            <Player.Button />
+                            <Player.Video src="/videos/bunny.mp4" />
+                        </Player>
                     </Card.Feature>
                 </Card>
             ))}
