1f29d922dc62785fb351fd3e7aed4ae6b8627893
Bernard Laughlin <bernardlaughlin@gmail.com>
2021-06-14 11:27:18 -0800

    removed cached files

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import {App} from './app';
-import {GlobalStyles} from './global-styles';
+import { render } from 'react-dom';
 import 'normalize.css';
-import { firebase } from './lib/firebase.prod'
-import { FirebaseContext } from './context/firebase'
-ReactDOM.render(
-<>
-  <FirebaseContext.Provider value={{ firebase}}>
-    <GlobalStyles />
-    <App /> 
-  </FirebaseContext.Provider>
-</>, document.getElementById('root'));
+import { GlobalStyles } from './global-styles';
+import { App } from './app';
+import { firebase } from './lib/firebase.prod';
+import { FirebaseContext } from './context/firebase';
 
-
+render(
+  <React.StrictMode>
+    <FirebaseContext.Provider value={{ firebase }}>
+      <GlobalStyles />
+      <App />
+    </FirebaseContext.Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
