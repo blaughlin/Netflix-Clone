042c6d1c83411261e9252cf3dfabcf7921be05a6
Bernard Laughlin <bernardlaughlin@gmail.com>
2021-06-15 10:33:56 -0800

    Removed API key

--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -6,12 +6,12 @@ import { seedDatabase } from '../seed'
 
 
 const config = {
-    apiKey: "AIzaSyAdwbEw7UpVpjdghgRF6BfBc35mHpi9rWM",
-    authDomain: "netflix-clone-942a7.firebaseapp.com",
-    projectId: "netflix-clone-942a7",
-    storageBucket: "netflix-clone-942a7.appspot.com",
-    messagingSenderId: "135010719718",
-    appId: "1:135010719718:web:03806d6f24b05dd410e827"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 }
 
 
